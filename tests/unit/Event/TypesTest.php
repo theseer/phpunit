97e2a7c44cf32a86433231d551e9ca9b1d15f452
Andreas Möller <am@localheinz.com>
2019-10-06 00:02:45 +0200

    Enhancement: Add test for Types

---
+++ tests/unit/Event/TypesTest.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Event;
+
+use NamedType;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \PHPUnit\Event\Types
+ */
+final class TypesTest extends TestCase
+{
+    public function testDefaults(): void
+    {
+        $types = new Types();
+
+        self::assertIsIterable($types);
+        self::assertEquals([], \iterator_to_array($types));
+    }
+
+    public function testConstructorSetsTypes(): void
+    {
+        $composedTypes = [
+            new NamedType('foo'),
+            new NamedType('bar'),
+            new NamedType('baz'),
+        ];
+
+        $types = new Types(...$composedTypes);
+
+        self::assertIsIterable($types);
+        self::assertEquals($composedTypes, \iterator_to_array($types));
+    }
+
+    public function testContainsReturnsFalseWhenEqualTypeHasNotBeenComposed(): void
+    {
+        $composedTypes = [
+            new NamedType('foo'),
+            new NamedType('bar'),
+            new NamedType('baz'),
+        ];
+
+        $type = new NamedType('qux');
+
+        $types = new Types(...$composedTypes);
+
+        self::assertFalse($types->contains($type));
+    }
+
+    public function testContainsReturnsTrueWhenEqualTypeHasBeenComposed(): void
+    {
+        $composedTypes = [
+            new NamedType('foo'),
+            new NamedType('bar'),
+            new NamedType('baz'),
+        ];
+
+        $type = new NamedType('bar');
+
+        $types = new Types(...$composedTypes);
+
+        self::assertTrue($types->contains($type));
+    }
+}
