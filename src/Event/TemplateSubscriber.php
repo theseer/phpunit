dc474c5f972fe42b490832fe470c07b7a435f6d0
Andreas Möller <am@localheinz.com>
2019-10-06 12:34:37 +0200

    Enhancement: Rename method

--- src/Event/TemplateSubscriber.php
+++ src/Event/TemplateSubscriber.php
@@ -22,7 +22,7 @@ abstract class TemplateSubscriber implements Subscriber
     private function ensureSupportedEventType(Event $event): void
     {
         if (!$this->typesSubscribedTo()->contains($event->type())) {
-            throw UnsupportedEvent::type(
+            throw UnexpectedEvent::for(
                 static::class,
                 $event->type()
             );
