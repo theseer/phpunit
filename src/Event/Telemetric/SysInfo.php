550fab2fd84fec9f42e048ea11aab93f614125eb
Andreas Möller <am@localheinz.com>
2019-11-14 12:22:12 +0100

    Fix: Run 'make cs'

--- src/Event/Telemetric/SysInfo.php
+++ src/Event/Telemetric/SysInfo.php
@@ -1,21 +1,30 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace PHPUnit\Event\Telemetric;
 
-class SysInfo {
-
+class SysInfo
+{
     /** @var Clock */
     private $clock;
 
     /** @var MemInfo */
     private $memInfo;
 
-    public function __construct(Clock $clock, MemInfo $memInfo) {
-        $this->clock = $clock;
+    public function __construct(Clock $clock, MemInfo $memInfo)
+    {
+        $this->clock   = $clock;
         $this->memInfo = $memInfo;
     }
 
-    public function snapshot(): Snapshot {
-
+    public function snapshot(): Snapshot
+    {
         return new SnapShot(
             $this->clock->now(),
             $this->memInfo->usage(),
