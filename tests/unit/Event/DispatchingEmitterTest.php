af02604722db8c2e89cf1babaa7dde26a633b5b5
Andreas Möller <am@localheinz.com>
2020-03-10 17:56:36 +0100

    Fix: Remove obsolete tests

--- tests/unit/Event/DispatchingEmitterTest.php
+++ tests/unit/Event/DispatchingEmitterTest.php
@@ -289,56 +289,6 @@ final class DispatchingEmitterTest extends Framework\TestCase
         $emitter->globalStateRestored();
     }
 
-    public function testAfterClassFinishedDispatchesAfterClassFinishedEvent(): void
-    {
-        $subscriber = $this->createMock(Test\AfterClassFinishedSubscriber::class);
-
-        $subscriber
-            ->expects($this->once())
-            ->method('notify')
-            ->with($this->isInstanceOf(Test\AfterClassFinished::class));
-
-        $dispatcher = self::createDispatcherWithRegisteredSubscriber(
-            Test\AfterClassFinishedSubscriber::class,
-            Test\AfterClassFinished::class,
-            $subscriber
-        );
-
-        $telemetrySystem = self::createTelemetrySystem();
-
-        $emitter = new DispatchingEmitter(
-            $dispatcher,
-            $telemetrySystem
-        );
-
-        $emitter->testAfterClassFinished();
-    }
-
-    public function testBeforeClassFinishedDispatchesBeforeClassFinishedEvent(): void
-    {
-        $subscriber = $this->createMock(Test\BeforeClassFinishedSubscriber::class);
-
-        $subscriber
-            ->expects($this->once())
-            ->method('notify')
-            ->with($this->isInstanceOf(Test\BeforeClassFinished::class));
-
-        $dispatcher = self::createDispatcherWithRegisteredSubscriber(
-            Test\BeforeClassFinishedSubscriber::class,
-            Test\BeforeClassFinished::class,
-            $subscriber
-        );
-
-        $telemetrySystem = self::createTelemetrySystem();
-
-        $emitter = new DispatchingEmitter(
-            $dispatcher,
-            $telemetrySystem
-        );
-
-        $emitter->testBeforeClassFinished();
-    }
-
     public function testTestRunConfiguredDispatchesTestRunConfiguredEvent(): void
     {
         $test                                       = $this->createMock(Framework\Test::class);
